bdfacfca309a41b2292290d84af22eed529c9983
Denis <Pensukdeath@yandex.ru>
2024-09-05 00:23:43 +0300

    Update Устранение дублей.cpp

--- 1.6/Устранение дублей.cpp
+++ 1.6/Устранение дублей.cpp
@@ -4,33 +4,29 @@
 
 using namespace std;
 
-void prints(int value, int power, int result) {
-
-  for (int i = 0; i < power; i++) {
-    result *= value;
-  }
-
-  cout << value << " в степени " << power << " = " << result << endl;
+double prints(int value, int power) {
+    int result = 1;
+    for (int i = 0; i < power; i++) {
+        result *= value;
+    }
+    return static_cast<double>(result);
 }
 
-int main(int argc, char **argv) {
-  void print();
+int main(int argc, char** argv) {
+    int value = 5;
+    int power = 2;
 
-  int value = 5, power = 2, result = 1;
+    int result = prints(value, power);
+    cout << value << " в степени " << power << " = " << result << endl;
+    value = 3;
+    power = 3;
 
-  prints(value, power, result);
+    result = prints(value, power);
+    cout << value << " в степени " << power << " = " << result << endl;
+    value = 4;
+    power = 4;
 
-  value = 3;
-  power = 3;
-  result = 1;
-
-  prints(value, power, result);
-
-  value = 4;
-  power = 4;
-  result = 1;
-
-  prints(value, power, result);
-
-  return 0;
+    result = prints(value, power);
+    cout << value << " в степени " << power << " = " << result << endl;
+    return 0;
 }
